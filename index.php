95b2e65daf97691e3a4219cd966f916c92da41d7
Tijani Moro <tjhackx111@gmail.com>
2023-07-24 10:33:46 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,290 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Highness - Maame Kwamba Acheampong</title>
+    <link rel="stylesheet" href="assets/css/vendor.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+ 
+  </head>
+  <body>
+
+
+    <!-- cover -->
+    <section class="p-0"
+      data-top-top="transform: translateY(0px);" 
+      data-top-bottom="transform: translateY(200px);">
+      <div class="swiper-container text-white">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide vh-100">
+            <div class="image image-overlay image-zoom" style="background-image:url(assets/media/img-1.jpg)"></div>
+            <div class="caption">
+              <div class="container">
+                <div class="row align-items-end justify-content-between vh-100">
+                  <div class="col-12 col-lg-6" data-swiper-parallax-y="-100%">
+                    <p class="lead mb-0">Hello, my name is</p>
+                    <h1 class="display-1 mb-2"><b>Maame Kwamba</b> Acheampong</h1>
+                    <p class="lead mb-10">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <div class="mouse"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- / cover -->
+
+
+    <section class="bg-red text-white"
+      data-top-top="transform: translateY(0px);" 
+      data-top-bottom="transform: translateY(200px);">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4">
+            <h2 class="fs-24 text-uppercase letter-spacing"><b>01.</b> skills</h2>
+          </div>
+          <div class="col-md-8">
+            <div class="row">
+              <div class="col-12">
+                <h4 class="progress-title">UI / UX Design</h4>
+                <div class="progress-item">
+                  <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 40%;" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"><span>40%</span></div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 mt-4">
+                <h4 class="progress-title">HTML / CSS / JS</h4>
+                <div class="progress-item">
+                  <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"><span>90%</span></div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 mt-4">
+                <h4 class="progress-title">Adobe Creative Suite</h4>
+                <div class="progress-item">
+                  <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 55%;" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"><span>55%</span></div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 mt-4">
+                <h4 class="progress-title">Social Media Marketing</h4>
+                <div class="progress-item">
+                  <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 65%;" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"><span>65%</span></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <section class="bg-dark text-white"
+      data-top-top="transform: translateY(0px);" 
+      data-top-bottom="transform: translateY(200px);">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4">
+            <h2 class="fs-24 text-uppercase letter-spacing"><b>02.</b> Experience</h2>
+          </div>
+          <div class="col-md-8 separated">
+            <div class="row">
+              <div class="col">
+                <div class="bordered separated">
+                  <div class="row mb-0 align-items-center justify-content-between p-2 p-md-4">
+                    <div class="col-3 col-md-2 pb-2 pb-md-0">
+                      <div class="logo">
+                        <img src="assets/media/logo-1.jpg" style="aspect-ratio: 3/2; object-fit: contain;" alt="Logo">
+                      </div>
+                    </div>
+                    <div class="col-md-5">
+                      <h5 class="mb-0">Founder</h5>
+                      <span>Mhikays Emporium</span>
+                    </div>
+                    <div class="col-md-4 text-md-right small">
+                      2017 - present
+                    </div>
+                  </div>
+                  <div class="row mb-0 align-items-center justify-content-between p-2 p-md-4">
+                    <div class="col-3 col-md-2 pb-2 pb-md-0">
+                      <div class="logo">
+                        <img src="assets/media/logo-2.png" alt="Logo">
+                      </div>
+                    </div>
+                    <div class="col-md-5">
+                      <h5 class="mb-0">Deputy WOCOM</h5>
+                      <span>TEIN</span>
+                    </div>
+                    <div class="col-md-4 text-md-right small">
+                      2023 - present
+                    </div>
+                  </div>
+                  <div class="row mb-0 align-items-center justify-content-between p-2 p-md-4">
+                    <div class="col-3 col-md-2 pb-2 pb-md-0">
+                      <div class="logo">
+                        <img src="../../assets/images/demo/logo/logo-3-white.png" alt="Logo">
+                      </div>
+                    </div>
+                    <div class="col-md-5">
+                      <h5 class="mb-0">Co -  Representative</h5>
+                      <span>Div/eloper</span>
+                    </div>
+                    <div class="col-md-4 text-md-right small">
+                      2022 - 2023
+                    </div>
+                  </div>
+                  <div class="row mb-0 align-items-center justify-content-between p-2 p-md-4">
+                    <div class="col-3 col-md-2 pb-2 pb-md-0">
+                      <div class="logo">
+                        <img src="assets/media/logo-3.jpg" alt="Logo">
+                      </div>
+                    </div>
+                    <div class="col-md-5">
+                      <h5 class="mb-0">Influencer</h5>
+                      <span>Twitter, Facebook, Instagram, Threads, etc</span>
+                    </div>
+                    <div class="col-md-4 text-md-right small">
+                      2017 - present
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- education -->
+    <section class="bg-white separator-top"
+      data-top-top="transform: translateY(0px);" 
+      data-top-bottom="transform: translateY(200px);">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4">
+            <h2 class="fs-24 text-uppercase letter-spacing"><b>03.</b> Education</h2>
+          </div>
+          <div class="col-md-8">
+            <div class="row gutter-1">
+              <div class="col-6">
+                <div class="bordered rising p-2 p-md-4">
+                  <h5 class="mb-0">University for Development Studies</h5>
+                  <span class="d-block text-primary mb-3">Dip. Computer Science</span>
+                  <span class="text-muted">2019 - 2021</span>
+                </div>
+              </div>
+              <div class="col-6">
+                <div class="bordered rising p-2 p-md-4">
+                  <h5 class="mb-0">CKT University of Technology and Applied Science</h5>
+                  <span class="d-block text-primary mb-3">Bsc. Computer Science</span>
+                  <span class="text-muted">2021 - Current</span>
+                </div>
+              </div>
+              <div class="col-6">
+                <div class="bordered rising p-2 p-md-4">
+                  <h5 class="mb-0">Amaniampong Senior High School</h5>
+                  <span class="d-block text-primary mb-3">Science</span>
+                  <span class="text-muted">2015 - 2018</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- / education -->
+
+
+    <section class="bg-light"
+      data-top-top="transform: translateY(0px);" 
+      data-top-bottom="transform: translateY(200px);">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4">
+            <h2 class="fs-24 text-uppercase letter-spacing"><b>04.</b> Portfolio</h2>
+          </div>
+          <div class="col-md-8">
+            <ul class="row gutter-1 gallery filtr-container">
+              <li class="col-6 filtr-item" data-category="all, branding" data-sort="value">
+                <figure class="photo equal">
+                  <a href="assets/media/img-4.jpg" 
+                    style="background-image: url(assets/media/img-4.jpg);">
+                  </a>
+                </figure>
+              </li>
+              <li class="col-6 filtr-item" data-category="all, illustration" data-sort="value">
+                <figure class="photo equal equal-long">
+                  <a href="assets/media/img-5.jpg" 
+                    style="background-image: url(assets/media/img-5.jpg);">
+                  </a>
+                </figure>
+              </li>
+              <li class="col-6 filtr-item" data-category="all, illustration" data-sort="value">
+                <figure class="photo equal equal-long">
+                  <a href="assets/media/img-2.jpg" 
+                    style="background-image: url(assets/media/img-2.jpg);">
+                  </a>
+                </figure>
+              </li>
+              <li class="col-6 filtr-item" data-category="all, branding" data-sort="value">
+                <figure class="photo equal">
+                  <a href="assets/media/img-1.jpg" 
+                    style="background-image: url(assets/media/img-1.jpg);">
+                  </a>
+                </figure>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <section class="bg-white">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            <h2>Get in touch with me! <a href="" class="text-red link">Write me</a></h2>
+          </div>
+        </div>
+      </div>
+    </section>
+
+ <!-- footer -->
+    <footer class="separator-top">
+      <div class="container py-5">
+        <div class="row justify-content-between align-items-center">
+          <div class="col-md-5 text-center text-md-left">
+          </div>
+          <div class="col-md-2 text-center">
+          </div>
+          <div class="col-md-5 text-center text-md-right">
+            <ul class="socials">
+              <li><a href="" class="icon-facebook text-muted fs-20"></a></li>
+              <li><a href="" class="icon-instagram text-muted fs-20"></a></li>
+              <li><a href="" class="icon-twitter text-muted fs-20"></a></li>
+              <li><a href="" class="icon-linkedin text-muted fs-20"></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- / footer -->
+
+
+    <script src="assets/js/vendor.js"></script>
+    <script src="assets/js/app.js"></script>
+  </body>
+</html>
